866423c418b4ad8adddad6ccfb60fa0abb10c28c
doeurnden32 <doeurnden32@gmail.com>
2023-06-01 20:12:25 +0700

    modifie code by build

--- dist/assets/CartView-b823dfe3.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,N as o,r as t,o as r,c as s,a as n,d as c,b as _}from"./index-5e352cbc.js";const l={components:{Navbar:o}},p=_("h1",null,"Hello From Cart Page",-1);function m(d,f,i,u,N,b){const e=t("Navbar");return r(),s(c,null,[n(e),p],64)}const $=a(l,[["render",m]]);export{$ as default};
